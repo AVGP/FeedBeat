260b9e2db5d6e1e152468797cc91125804f671fa
Martin N <mr.avgp@googlemail.com>
2014-03-29 11:19:32 -0400

    Initial commit

---
+++ js/controllers.js
@@ -0,0 +1,18 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.feeds = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('FavouritesCtrl', function($scope) {
+})
+
+.controller('AccountCtrl', function($scope) {
+});
